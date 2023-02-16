0c8c80eb0b6d336cba57a75f5edcabb24bc68867
kumawatlalit007 <kumawatlalit912@gmail.com>
2023-02-16 16:44:19 +0530

    update-by-post-data_score

--- backend/newServer.js
+++ backend/newServer.js
@@ -15,17 +15,47 @@ const someData = async (name) => {
         query: {
           match: {
             Series_Title: {
-              query: `${name}`,
+              query: `{$name}`,
               analyzer: "standard",
+              fuzziness: 2,
               prefix_length: 1,
             },
+          },
+        },
+        sort: {
+          doc_Score: {
+            order: "desc",
+          },
+        },
+      },
+    });
+    console.log(some.hits.hits);
+    return some.hits.hits;
+  } catch (e) {
+    console.error("error while connecting to Elasticsearch", e.stack);
+  }
+};
+const updateData = async (name, id) => {
+  try {
+    const some = await client.updateByQuery({
+      index: "movies",
+      body: {
+        query: {
+          term: {
+            _id: id,
+          },
+        },
+        script: {
+          source: "ctx._source.data_Score =params.newDataScore",
+          params: {
+            newDataScore: "Math.log10(doc['data_score'].value+1)",
           },
         },
       },
     });
     return some.hits.hits;
   } catch (e) {
-    console.error("error while connecting to Elasticsearch", e.stack);
+    console.log("some error occurred while updating", e);
   }
 };
 
@@ -38,6 +68,14 @@ app.get("/data/:name", async (req, res) => {
     res.send(chunkData);
   } catch (e) {
     res.send("hola amigo");
+  }
+});
+app.post("/data/:name/:id", async (req, res) => {
+  try {
+    const somedata = await updateData(req.params.name, req.params.id);
+    res.send(somedata);
+  } catch (e) {
+    res.send("some error occured while updating data");
   }
 });
 app.listen(3000, console.log("server is listening on port 3000"));
